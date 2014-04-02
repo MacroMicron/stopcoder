78b6f4a2ce482fbb69113675badc67cbbe231476
MacroMicron <northwolf@bk.ru>
2014-04-02 18:06:10 +0300

    Solution for 3th problem upgraded.
    Added smart using of time.
    1. We read all tasks and perform tasks at first with small string length.
    2. If we spend all time which have then break and print "zaglushkis".
    Last point give me 70/200 scores.

--- 2014-march/3-nucleotides-sequence/main.c
+++ 2014-march/3-nucleotides-sequence/main.c
@@ -1,15 +1,13 @@
+// This program give me 70/200 score on Samsung Top Coder Contest.
+//
 // Simple algo with molecule and rolecule.
 // Molecule string fixed and rolecule string we shift right 2*N-1 times.
 // O(N^2) char comparsions.
 // 
 // This algo takes many time.
-// On test/myhard.txt we have
-// Case #1
-// 1 999999
-//
-// real    299m54.518s
-// user    299m32.455s
-// sys     0m5.724s
+// Smart time feature use:
+// 1. we read all tasks and perform first tasks with short length
+// 2. if we spend all time then break and print "zaglushki"
 
 /*
 In S-Topcoder Contest,
@@ -24,27 +22,44 @@ Please be very careful.
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#define BILLION  1000000L
 
-char data[1000000];
+
+inline double timedelta(struct timeval start_time, struct timeval end_time)
+{
+	return (end_time.tv_sec - start_time.tv_sec) + (double)(end_time.tv_usec - start_time.tv_usec) / (double)BILLION;
+}
 
 typedef struct Pair_
 {
-	long fst;
-	long snd;
+	long fst; // length in this task
+	long snd; // first index in this task
 } Pair;
 
 struct Molecule_
 {
 	long length, first;
 	int RL; //RL: if from right to left -1 and left to right = 1
+	char *data;
 };
 typedef struct Molecule_* Molecule;
+
+typedef struct TestCase_
+{
+        Molecule molecule;
+        Molecule rolecule;
+        char *data;
+        Pair answer; // Answer.fst is a length and Answer.snd is a first letter index
+	int number;
+} TestCase;
 
 // 0 <= i <= length-1
 inline char get_dnk(Molecule molecule, long i)
 {
 	return ((molecule->first+molecule->RL*i >= 0)&&(molecule->first+molecule->RL*i<molecule->length)) ? 
-			data[molecule->first+molecule->RL*i] : 'X';
+			molecule->data[molecule->first+molecule->RL*i] : 'X';
 }
 
 void shift_right(Molecule molecule)
@@ -75,20 +90,20 @@ Pair compare(Molecule molecule1, Molecule molecule2)
 		if ((ch != 'X') && (get_dnk(molecule1, i) == ch))
 		{
 			current.fst++;
-			if (current.snd==0) current.snd=i+1;
+			if (current.snd==0) current.snd=i+1; //indexes begin from 1
 		}
 		else
 		{
-			if (current.fst > total.fst) {total.fst = current.fst; total.snd = current.snd;}
+			if (current.fst > total.fst) total = current;
 			current.fst = 0;
 			current.snd = 0;
 		}
 	}
-	if (current.fst > total.fst) {total.fst = current.fst; total.snd = current.snd;}
+	if (current.fst > total.fst) total = current;
 	return total;
 }
 
-void print(Molecule molecule)
+void printm(Molecule molecule)
 {
 	long i;
 	for (i=0; i<molecule->length; i++)
@@ -98,70 +113,127 @@ void print(Molecule molecule)
 	printf("\n");
 }
 
+void swap_tests(TestCase *test1, TestCase *test2)
+{
+		TestCase temp = {test1->molecule, test1->rolecule, test1->data, test1->answer, test1->number};
+		test1->molecule = test2->molecule;
+		test1->rolecule = test2->rolecule;
+		test1->data = test2->data;
+		test1->answer = test2->answer;
+		test1->number = test2->number;
+		
+		test2->molecule = temp.molecule;
+		test2->rolecule = temp.rolecule;
+		test2->data = temp.data;
+		test2->answer = temp.answer;
+		test2->number = temp.number;
+}
+
+// return index of longest test
+int longest_test(TestCase *tests, int T)
+{
+	if (T==1) return 0;
+	int index = 1+longest_test(&tests[1], T-1);
+	return tests[0].molecule->length > tests[index].molecule->length ? 0 : index;
+}
+
+void prints(TestCase *tests, int T)
+{
+        int test_case;
+        for (test_case = 0; test_case < T; test_case++)
+        {
+                printf("testcase[%d]: length %ld\n", test_case, tests[test_case].molecule->length);
+        }
+}
+
+// intellectual sorting tests from simple to hard
+void sort_tests(TestCase *tests, int T)
+{
+	if (T==1) return;
+	int index = longest_test(tests, T);
+	if (index != T-1) swap_tests(&tests[index], &tests[T-1]);
+	sort_tests(tests, T-1);
+}
+
+
+//========================================================================
+
+
+// global variables
+char **all_data = NULL;
+TestCase *tests = NULL;
+int T = 0; //number of tests
+struct timeval start_time, current_time;
+struct timezone start_zone, current_zone;
+double timeout=2.99;
+int timeoff = 0; // 1 - if time expired and 0 if didn't
+
 int main(void)
 {
-	int T, test_case;
+	gettimeofday(&start_time, &start_zone);
+	int test_case, index;
+	TestCase *test = NULL;
+	char *data = NULL;
 	long number_of_dnk;
-	long Answer, First;
 	Pair temp;
-	struct Molecule_ molecule_, rolecule_;
-	Molecule molecule = &molecule_, rolecule = &rolecule_;
-	/*
-	   The freopen function below opens input.txt file in read only mode, and afterward,
-	   the program will read from input.txt file instead of standard(keyboard) input.
-	   To test your program, you may save input data in input.txt file,
-	   and use freopen function to read from the file when using scanf function.
-	   You may remove the comment symbols(//) in the below statement and use it.
-	   But before submission, you must remove the freopen function or rewrite comment symbols(//).
-	 */
-	// freopen("input.txt", "r", stdin);
-
-	/*
-	   If you remove the statement below, your program's output may not be rocorded
-	   when your program is terminated after the time limit.
-	   For safety, please use setbuf(stdout, NULL); statement.
-	 */
+	
 	setbuf(stdout, NULL);
-
 	scanf("%d", &T);
+	tests = (TestCase*) malloc(T*sizeof(TestCase));
+	all_data = (char **) malloc(T*sizeof(char*));
+	// read all tasks
+        for(test_case = 0; test_case < T; test_case++)
+        {
+                test = &tests[test_case];
+                test->molecule = (Molecule) malloc(sizeof(struct Molecule_));
+                test->rolecule = (Molecule) malloc(sizeof(struct Molecule_));
+                test->answer.fst = 1;
+                test->answer.snd = 1;
+		test->number = test_case + 1;
+		scanf("%ld", &test->molecule->length);
+                test->rolecule->length = test->molecule->length;
+		all_data[test_case] = (char *) malloc(test->molecule->length * sizeof(char));
+		data = test->molecule->data = test->rolecule->data = all_data[test_case];
+		scanf("%s", data);
+                test->rolecule->first = test->molecule->first = 0;
+                test->rolecule->RL = test->molecule->RL = 1;
+                reverse(test->rolecule);
+		test->rolecule->first -= test->rolecule->length;
+	}
 	for(test_case = 0; test_case < T; test_case++)
 	{
-		
-		/////////////////////////////////////////////////////////////////////////////////////////////
-		/*
-		   Implement your algorithm here.
-		   The answer to the case will be stored in variable Answer.
-		 */
-		/////////////////////////////////////////////////////////////////////////////////////////////
-		Answer = 0;
-		First = 0;
-		scanf("%ld", &molecule->length);
-		rolecule->length = molecule->length;
-		scanf("%s", data);
-		rolecule->first = molecule->first = 0;
-		rolecule->RL = molecule->RL = 1;
-		reverse(rolecule);
-		rolecule->first -= rolecule->length;
-		for (number_of_dnk = 0; number_of_dnk < 2*molecule->length-1; number_of_dnk++)
+		test = &tests[test_case];
+		if (!timeoff) for (number_of_dnk = 0; number_of_dnk < 2 * test->molecule->length-1 ; number_of_dnk++)
 		{	
-			shift_right(rolecule);
-			temp = compare(molecule, rolecule);
-			if (temp.fst>Answer) {Answer=temp.fst;	First=temp.snd;}
-			//print(molecule);
-			//print(rolecule);
-			//printf("%ld %ld\n", temp.fst, temp.snd);
+			shift_right(test->rolecule);
+			temp = compare(test->molecule, test->rolecule);
+			if (temp.fst > test->answer.fst) test->answer = temp;
+			gettimeofday(&current_time, &current_zone);
+			if ( timeout <= timedelta(start_time, current_time) ) {timeoff = 1; break;} 
 		}
-		
-		
-		
-		
-		
-
-		// Print the answer to standard output(screen).
-		printf("Case #%d\n", test_case+1);
-		printf("%ld %ld\n", First, Answer);
 	}
 
-	return 0;//Your program should return 0 on normal termination.
+	for(test_case = 1; test_case <= T; test_case++)
+	{
+		index = -1;
+		do {
+			index++;
+			test = &tests[index];
+		} while (test->number != test_case);
+		// Print the answer to standard output(screen).
+                printf("Case #%d\n", test->number);
+                printf("%ld %ld\n", test->answer.snd, test->answer.fst);
+	}
+	
+        for(test_case = 0; test_case < T; test_case++)
+	{
+                test = &tests[test_case];
+		free(test->molecule->data);
+                free(test->molecule);
+                free(test->rolecule);
+	}
+	free(all_data);
+	free(tests);
+	return 0; //Your program should return 0 on normal termination.
 }
 
